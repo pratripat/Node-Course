a41c605a3f203f91d0018105cead117f2cd8e946
Pratyush Ram Tripathi <pratyusht2006@gmail.com>
2025-07-12 16:11:49 +0530

    Removed all node_modules
    Signed-off-by: Pratyush Ram Tripathi <pratyusht2006@gmail.com>

--- vidly/tests/unit/models/auth.test.js
+++ vidly/tests/unit/models/auth.test.js
@@ -1,9 +1,14 @@
 const { User } = require('../../../models/user');
 const auth = require('../../../middleware/auth');
+const mongoose = require('mongoose');
 
 describe('auth middleware', () => {
     it('should populate req.user with the payload of a valid JWT', () => {
-        const token = new User().generateAuthToken();
+        const user = { 
+            _id: new mongoose.Types.ObjectId().toHexString(),
+            isAdmin: true
+        };
+        const token = new User(user).generateAuthToken();
         const req = {
             header: jest.fn().mockReturnValue(token)
         }
@@ -12,6 +17,6 @@ describe('auth middleware', () => {
 
         auth(req, res, next);
 
-        expect(req.user).toBeDefined();
+        expect(req.user).toMatchObject(user);
     });
 });
